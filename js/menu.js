6ade533e86d717a56f5926b78a2aaedd7db6bd30
proyectoswebtx <79871767+proyectoswebtx@users.noreply.github.com>
2021-03-02 14:53:26 -0400

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+let btnMenu=document.getElementById('btnmenu');
+let Menu=document.getElementById('menu');
+btnMenu.addEventListener('click',function(){
+    'use strict'
+    Menu.classList.toggle('mostrar');
+});
